bbbe1ef1e5e5d0e3c42bb7164c53abc0ee0393dc
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2026-03-09 18:58:34 +0100

    Themes - Under construction

--- hmi/dev/scripts/generate_images.sh
+++ hmi/dev/scripts/generate_images.sh
@@ -48,10 +48,43 @@ for i in {1..15}; do
   fi
 done
 
-# 19,20 Cover controls
-# 39,40 Fan settings
+# 17 light.colorwheel
+# 19,20 cover.coverslider
+# 21 light.tempslider
+# 22,23 light.light_b_press
+# 24,25 light.temp_b_press
+# 26,27 light.color_b_pres
+# 28,29 light.lightslider
+if [[ $VARIANT = eu* ]]; then
+  magick_command "$VARIANT/0.png" "pics/17.png" -geometry +106+68 -composite -background black -flatten -crop 200x200+106+68 +repage "$VARIANT/17.png"
+  magick_command "$VARIANT/0.png" "pics/19.png" -geometry +183+67 -composite -background black -flatten -crop 83x233+183+67 +repage "$VARIANT/19.png"
+  magick_command "$VARIANT/0.png" "pics/20.png" -geometry +183+67 -composite -background black -flatten -crop 83x233+183+67 +repage "$VARIANT/20.png"
+  magick_command "$VARIANT/0.png" "pics/21.png" -geometry +164+67 -composite -background black -flatten -crop 85x235+164+67 +repage "$VARIANT/21.png"
+  magick_command "$VARIANT/0.png" "pics/22.png" -geometry +389+74 -composite -background black -flatten -crop 50x50+389+74 +repage "$VARIANT/22.png"
+  magick_command "$VARIANT/0.png" "pics/23.png" -geometry +389+74 -composite -background black -flatten -crop 50x50+389+74 +repage "$VARIANT/23.png"
+  magick_command "$VARIANT/0.png" "pics/24.png" -geometry +389+138 -composite -background black -flatten -crop 50x50+389+138 +repage "$VARIANT/24.png"
+  magick_command "$VARIANT/0.png" "pics/25.png" -geometry +389+138 -composite -background black -flatten -crop 50x50+389+138 +repage "$VARIANT/25.png"
+  magick_command "$VARIANT/0.png" "pics/26.png" -geometry +389+201 -composite -background black -flatten -crop 50x50+389+201 +repage "$VARIANT/26.png"
+  magick_command "$VARIANT/0.png" "pics/27.png" -geometry +389+201 -composite -background black -flatten -crop 50x50+389+201 +repage "$VARIANT/27.png"
+  magick_command "$VARIANT/0.png" "pics/28.png" -geometry +164+67 -composite -background black -flatten -crop 85x235+164+67 +repage "$VARIANT/28.png"
+  magick_command "$VARIANT/0.png" "pics/29.png" -geometry +164+67 -composite -background black -flatten -crop 85x235+164+67 +repage "$VARIANT/29.png"
+else
+  magick_command "$VARIANT/0.png" "pics/17.png" -geometry +61+211 -composite -background black -flatten -crop 200x200+61+211 +repage "$VARIANT/17.png"
+  magick_command "$VARIANT/0.png" "pics/19.png" -geometry +119+143 -composite -background black -flatten -crop 83x233+119+143 +repage "$VARIANT/19.png"
+  magick_command "$VARIANT/0.png" "pics/20.png" -geometry +119+143 -composite -background black -flatten -crop 83x233+119+143 +repage "$VARIANT/20.png"
+  magick_command "$VARIANT/0.png" "pics/21.png" -geometry +117+194 -composite -background black -flatten -crop 85x235+117+194 +repage "$VARIANT/21.png"
+  magick_command "$VARIANT/0.png" "pics/22.png" -geometry +25+71 -composite -background black -flatten -crop 50x50+25+71 +repage "$VARIANT/22.png"
+  magick_command "$VARIANT/0.png" "pics/23.png" -geometry +25+71 -composite -background black -flatten -crop 50x50+25+71 +repage "$VARIANT/23.png"
+  magick_command "$VARIANT/0.png" "pics/24.png" -geometry +134+71 -composite -background black -flatten -crop 50x50+134+71 +repage "$VARIANT/24.png"
+  magick_command "$VARIANT/0.png" "pics/25.png" -geometry +134+71 -composite -background black -flatten -crop 50x50+134+71 +repage "$VARIANT/25.png"
+  magick_command "$VARIANT/0.png" "pics/26.png" -geometry +243+71 -composite -background black -flatten -crop 50x50+243+71 +repage "$VARIANT/26.png"
+  magick_command "$VARIANT/0.png" "pics/27.png" -geometry +243+71 -composite -background black -flatten -crop 50x50+243+71 +repage "$VARIANT/27.png"
+  magick_command "$VARIANT/0.png" "pics/28.png" -geometry +117+194 -composite -background black -flatten -crop 85x235+117+194 +repage "$VARIANT/28.png"
+  magick_command "$VARIANT/0.png" "pics/29.png" -geometry +117+194 -composite -background black -flatten -crop 85x235+117+194 +repage "$VARIANT/29.png"
+fi
 
-for i in 19 20 39 40; do
+# 39,40 Fan settings
+for i in 39 40; do
   if [[ $VARIANT = eu* ]]; then
     magick_command "$VARIANT/0.png" "pics/$i.png" -geometry +183+67 -composite -background black -flatten -crop 83x233+183+67 +repage "$VARIANT/$i.png"
   else
@@ -59,10 +92,11 @@ for i in 19 20 39 40; do
   fi
 done
 
+# 32 Splash screen
 # 33,34 Settings page
 # 37 Climate controls
 # 46,47 Home Alt
-for i in 33 34 37 46 47; do
+for i in 32 33 34 37 46 47; do
   if [[ $VARIANT = eu* ]]; then
     magick_command "$VARIANT/0.png" "pics/$i.png" -geometry +0+0 -composite -background black -flatten -crop 480x320 +repage "$VARIANT/$i.png"
   else
@@ -72,22 +106,12 @@ done
 
 # Keep these with black background, either as they are on a black screen or they don't have transparency (e.g. sliders)
 
-# 16: Slider on Settings page
-# 17: Colour page
-# 18: Slider on Cover, Fan & Colour pages
-# 21-29: Colour page
-# 30,31: Indicators
+# 16: Slider cursor on Settings page
+# 18: Slider cursor on Cover, Fan & Light pages
+# 30,31: Indicators - Not in use
 # 38: Slider on Climate page
 # 44,45: Alarm
-
 # 35,36,41,42,43: Unused?
-for i in {16..18} {21..31} 35 36 38 {41..45}; do
+for i in 16 18 {30..31} 35 36 38 {41..45}; do
   cp "${IMAGES_DIR}/pics/$i.png" "${IMAGES_DIR}/$VARIANT/$i.png"
 done
-
-# 32: Splash screen (JPG!)
-if [[ $VARIANT = eu* ]]; then
-  cp "${IMAGES_DIR}/pics/32.jpg" "${IMAGES_DIR}/$VARIANT/32.jpg"
-else
-  cp "${IMAGES_DIR}/pics/32-portrait.jpg" "${IMAGES_DIR}/$VARIANT/32.jpg"
-fi
