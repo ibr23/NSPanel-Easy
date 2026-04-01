e5f83092d71d523f85c7378a66d0ebfb7ab6ee90
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2026-04-01 17:30:49 +0200

    New blueprint versioning

--- components/nspanel_easy/versioning.cpp
+++ components/nspanel_easy/versioning.cpp
@@ -5,12 +5,7 @@
 #include "versioning.h"
 
 namespace esphome {
-namespace nspanel_easy {
-
-uint8_t version_blueprint = 0;
-uint8_t version_tft = 0;
-
-}  // namespace nspanel_easy
+namespace nspanel_easy { }  // namespace nspanel_easy
 }  // namespace esphome
 
 #endif  // NSPANEL_EASY_VERSIONING
