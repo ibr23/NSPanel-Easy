c27bae7e6f1c9ec5d6c2c8986d045e659b00981e
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2026-04-13 20:15:43 +0200

    Move headers to component
    https://github.com/edwardtfn/NSPanel-Easy/pull/118#pullrequestreview-4095850791

---
+++ components/nspanel_easy/hw_memory.h
@@ -0,0 +1,13 @@
+// hw_memory.h
+
+#pragma once
+
+#ifdef NSPANEL_EASY_HW_MEMORY
+
+#include <esp_heap_caps.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+namespace esphome::nspanel_easy {}  // namespace esphome::nspanel_easy
+
+#endif  // NSPANEL_EASY_HW_MEMORY
